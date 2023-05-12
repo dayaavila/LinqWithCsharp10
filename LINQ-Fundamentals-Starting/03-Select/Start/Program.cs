e92703f9a1c70137660524026329f665371f0d64
Dayana Avila <dayaavila87@gmail.com>
2023-05-12 13:57:32 +0200

    first commit

---
+++ LINQ-Fundamentals-Starting/03-Select/Start/Program.cs
@@ -0,0 +1,54 @@
+﻿using LINQSamples;
+
+// // Create instance of view model
+// SamplesViewModel vm = new();
+
+// // Call Sample Method
+// var result = vm.GetAllQuery();
+
+// // Display Results
+// vm.Display(result);
+
+//---------------
+
+// // Create instance of view model
+// SamplesViewModel vm2 = new();
+
+// // Call Sample Method
+// var result2 = vm2.GetAllMethod();
+
+// // Display Results
+// vm2.Display(result2);
+
+//---------------
+
+// // Create instance of view model
+// SamplesViewModel vm3 = new();
+
+// // Call Sample Method
+// var result3 = vm3.GetSingleColumnQuery();
+
+// // Display Results
+// vm3.Display(result3);
+
+// //---------------
+
+// // Create instance of view model
+// SamplesViewModel vm4 = new();
+
+// // Call Sample Method
+// var result4 = vm4.GetSingleColumnMethod();
+
+// // Display Results
+// vm4.Display(result4);
+
+//---------------
+
+// Create instance of view model
+SamplesViewModel vm5 = new();
+
+// Call Sample Method
+var result5 = vm5.GetSpecificColumnsMethod();
+
+// Display Results
+vm5.Display(result5);
